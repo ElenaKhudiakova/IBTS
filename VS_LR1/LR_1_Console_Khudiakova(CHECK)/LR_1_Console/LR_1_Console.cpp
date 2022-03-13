bd08ac52b7c1db20692d386b20a484cd1ae3ceb9
Khudiakova Elena <87654857+ElenaKhudiakova@users.noreply.github.com>
2022-03-13 16:14:38 +0500

    Add files via upload

---
+++ VS_LR1/LR_1_Console_Khudiakova(CHECK)/LR_1_Console/LR_1_Console.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <LR_1.h>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Лабораторная работа номер 1:DLL\n\n";
+    std::cout << "Номер группы: " << Group() << "\n";
+    std::cout << "ФИО: "; FIO();
+    std::cout << "\n";
+    system("pause");
+}
